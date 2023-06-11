1fa6f0614cfbb74470432f950c051f8ff8793f7d
Giorgia <121100010+Giorgias05@users.noreply.github.com>
2023-06-11 17:20:22 +0200

    Add files via upload

---
+++ backEnd_w5_project/src/main/java/com/backEnd_w5_project/auth/entity/StatoDispositivo.java
@@ -0,0 +1,6 @@
+package com.backEnd_w5_project.auth.entity;
+
+public enum StatoDispositivo {
+DISPONIBILE,ASSEGNATO,DISMESSO,IN_MANUTENZIONE
+
+}
---
+++ backEnd_w5_project/src/main/java/com/backEnd_w5_project/auth/entity/TipoDispositivo.java
@@ -0,0 +1,5 @@
+package com.backEnd_w5_project.auth.entity;
+
+public enum TipoDispositivo {
+	LAPTOP,TABLET,SMARTPHONE,
+}
